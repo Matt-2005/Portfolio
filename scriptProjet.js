d47fdced43307067358ad3f518bda2653da94453
Matthew LE <matthew.le548@gmail.com>
2025-02-28 17:08:54 +0100

    finalisation

--- scriptProjet.js
+++ scriptProjet.js
@@ -2,17 +2,10 @@ new Swiper('.card-wrapper', {
     loop: true,
     spaceBetween: 30,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-    },
-
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.projet2Arrow.next',
+        prevEl: '.projet2Arrow.prev ',
     },
     breakpoints: {
         0: {
@@ -25,10 +18,10 @@ new Swiper('.card-wrapper', {
             slidesPerView: 3
         },  
     },
-    autoplay: {
-        delay: 3000,
-        pauseOnMouseEnter: true,
-    },
+    // autoplay: {
+    //     delay: 3000,
+    //     pauseOnMouseEnter: true,
+    // },
 
 });
 
